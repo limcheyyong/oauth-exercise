901f98452f2093a4f8e05cf8d13f8a132fcba42b
林則勇 Yong <yong@miniasp.com>
2022-07-02 13:22:03 +0800

    production enviroment 調整

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   clientIdNotify: 'U9UUaxoXcDfFMsBq0kFj28',
-  clientIdLineLogin: 1657269117,
-  dummyClientSecret: 'O77gpnpxmehpoD0L0UHsTDjKC3ZTyxQFWcasADU1Ykp',
+  clientIdLineLogin: '1657269117',
+  clientSecretNotify: 'O77gpnpxmehpoD0L0UHsTDjKC3ZTyxQFWcasADU1Ykp',
+  clientSecretLineLogin: '1d670b71cfe946a543b51b1bd80787e7',
 };
